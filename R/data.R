6329e5cb3ecae8e3bc66f935ca2a6a85abf17407
MariekeDirk <marieke_dirksen@hotmail.com>
2018-02-01 16:05:58 +0000

    example of the documentation: needs some improvements

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#'Car Data
+#'
+#'This dataset contains information of the color, speed and state of cars.
+#'
+#'@format A data frame with 100 rows and 3 variables
+#'\describe{
+#' \item{Color}{The color of the car. These are characters}
+#' \item{Speed}{The maximum speed of the car in m/s}
+#' \item{State}{The state where the car was made}
+#' }
+"car_data"
---
+++ data-raw/dataraw.R
@@ -0,0 +1,4 @@
+
+car_data<-fread("_episodes_rmd/data/car-speeds.csv")
+
+devtools::use_data(car_data)
